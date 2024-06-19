c9728d81760c0e42748a3b460b453882a8fc3b9d
Shivan Kathir <shivan.kathir@gmail.com>
2024-06-18 21:08:24 -0400

    added a restart button that restarts game

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const gameboard = function(){
-    const board =[null,null,null,null,null,null,null,null,null];
+    let board =[null,null,null,null,null,null,null,null,null];
     function pushToBoard(symbol,index){
         board[index]=symbol;
     }
@@ -8,39 +8,43 @@ const gameboard = function(){
         return board[index];
     }
 
+    function resetBoard(){
+        board=[null,null,null,null,null,null,null,null,null];
+    }
+
     
 
     function checkWin(symbol){
         if (board[0]===symbol&&board[1]===symbol&&board[2]===symbol){
-            console.log("You won");
+        
             return true;
         }
         else if(board[3]===symbol&&board[4]===symbol&&board[5]===symbol){
-            console.log("1");
+           
             return true;
         }
         else if(board[6]===symbol&&board[7]===symbol&&board[8]===symbol){
-            console.log("2");
+           
             return true;
         }
         else if(board[0]===symbol&&board[4]===symbol&&board[8]===symbol){
-            console.log("3");
+            
             return true;
         }
         else if(board[2]===symbol&&board[4]===symbol&&board[6]===symbol){
-            console.log("4");
+            
             return true;
         }
         else if(board[0]===symbol&&board[3]===symbol&&board[6]===symbol){
-            console.log("5");
+           
             return true;
         }
         else if(board[1]===symbol&&board[4]===symbol&&board[7]===symbol){
-            console.log("6");
+          
             return true;
         }
         else if(board[2]===symbol&&board[5]===symbol&&board[8]===symbol){
-            console.log("7");
+            
             return true;
         }
         else{
@@ -49,7 +53,7 @@ const gameboard = function(){
     }
 
 
-    return {pushToBoard,getSymbolAt,checkWin};
+    return {pushToBoard,getSymbolAt,checkWin,resetBoard};
 }();
 
 function player(name,symbol){
@@ -82,7 +86,10 @@ const displayControl = function(){
     let win=false;
     let numOfTurns =0;
 
-    document.querySelectorAll(".grid").forEach((index)=>{
+
+
+    const board = document.querySelectorAll(".grid");
+    board.forEach((index)=>{
         index.addEventListener("click",()=>{
             if (index.textContent===""&&!win){
                 index.textContent=symbol;
@@ -106,17 +113,33 @@ const displayControl = function(){
 
             if (win===true&&symbol==="O"){
                 winMessage.textContent=`${player1.getPlayerName()} is the winner!`;
+                
             }
             else if (win===true &&symbol==="X"){
                 winMessage.textContent=`${player2.getPlayerName()} is the winner!`;
 
             }
             else if (numOfTurns===9){
-                winMessage.textContent="Oh No, It's a tie"
+                winMessage.textContent="Oh No, It's a tie!"
             }
         })
     })
+    
+    const restart = document.querySelector(".restart");
+    restart.addEventListener("click",()=>{
+        board.forEach((index)=>{
+            index.textContent="";
+            win=false;
+            gameboard.resetBoard();
 
+        });
+    });
+
+
+    
+}();
+
+const restartGame = function(){
     
 }();
 
